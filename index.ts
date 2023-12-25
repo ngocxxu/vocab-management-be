6cade0405ecc6021832cf050a5df0595dad537f9
bono43 <bono43@nexondv.com>
2023-12-25 17:38:41 +0700

    config connected

--- index.ts
+++ index.ts
@@ -6,18 +6,9 @@ import comment from './app/routers/Comment.routers.ts';
 dotenv.config();
 const app = express();
 
-app.use(cors());
-app.use(express.json({ limit: '30mb' }));
-app.use(express.urlencoded({ extended: true, limit: '30mb' }));
-
-app.use('/api/comment', comment);
-
-// console.log('hi', process.env.DATABASE_URL);
-
 mongoose
   .connect(
-    process.env.DATABASE_URL ??
-      'mongodb+srv://bono:ngoc25@cluster0.rbdq7.mongodb.net/vocab_management_db'
+    'mongodb+srv://bono:ngoc25@cluster0.rbdq7.mongodb.net/vocab_management_db'
   )
   .then(() => {
     console.log('Connected to DB');
