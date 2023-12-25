710f83106951a029148af62898e9ee621fc69e4b
bono43 <bono43@nexondv.com>
2023-12-25 13:00:22 +0700

    remove nodemon

--- index.ts
+++ index.ts
@@ -11,20 +11,16 @@ app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ extended: true, limit: '30mb' }));
 
 app.use('/api/comment', comment);
-console.log('env: ', process.env.DATABSE_URL);
+// console.log('env: ', process.env.DATABSE_URL);
 mongoose
   .connect(
     // process.env.DATABASE_URL ??
-    'mongodb+srv://bono:ngoc25@cluster0.rbdq7.mongodb.net/vocab_management_db',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    } as ConnectOptions
+    'mongodb+srv://bono:ngoc25@cluster0.rbdq7.mongodb.net/vocab_management_db'
   )
   .then(() => {
     console.log('Connected to DB');
     app.listen(process.env.LOCAL_PORT, () => {
-      console.log(`Server is running on port ${process.env.LOCAL_PORT}`);
+      console.log(`Server is running on port 4030`);
     });
   })
   .catch((err) => {
