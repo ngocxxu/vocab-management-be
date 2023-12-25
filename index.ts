e841a27d403083bff20b9ebb5b29d81db8e7b07c
bono43 <bono43@nexondv.com>
2023-12-25 15:08:58 +0700

    comment 2

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 import cors from 'cors';
-import 'dotenv/config';
+import * as dotenv from 'dotenv';
 import express from 'express';
-import mongoose, { ConnectOptions } from 'mongoose';
+import mongoose from 'mongoose';
 import comment from './app/routers/Comment.routers.ts';
-require('dotenv').config();
+dotenv.config();
 const app = express();
 
 app.use(cors());
@@ -11,10 +11,13 @@ app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ extended: true, limit: '30mb' }));
 
 app.use('/api/comment', comment);
+
+// console.log('hi', process.env.DATABASE_URL);
+
 mongoose
   .connect(
-    // process.env.DATABASE_URL ??
-    'mongodb+srv://bono:ngoc25@cluster0.rbdq7.mongodb.net/vocab_management_db'
+    process.env.DATABASE_URL ??
+      'mongodb+srv://bono:ngoc25@cluster0.rbdq7.mongodb.net/vocab_management_db'
   )
   .then(() => {
     console.log('Connected to DB');
