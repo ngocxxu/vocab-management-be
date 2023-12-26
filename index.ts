bec20ffdec23d2e3b5a13b0dbeda71bc63ba4724
ngocxxu <ngocquach43@gmail.com>
2023-12-27 00:15:16 +0700

    config env 1

--- index.ts
+++ index.ts
@@ -3,13 +3,19 @@ import * as dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 import comment from './app/routers/Comment.routers.ts';
+
 dotenv.config();
+
 const app = express();
 
+app.use(cors());
+app.use(express.json({ limit: '30mb' }));
+app.use(express.urlencoded({ extended: true, limit: '30mb' }));
+
+app.use('/api/comment', comment);
+
 mongoose
-  .connect(
-    'mongodb+srv://bono:ngoc25@cluster0.rbdq7.mongodb.net/vocab_management_db'
-  )
+  .connect(process.env.DATABASE_URL || '')
   .then(() => {
     console.log('Connected to DB');
     app.listen(4030, () => {
