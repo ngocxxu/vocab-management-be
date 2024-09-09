50b1853a6e2c890efc347d4c4fd5e58749a981e9
NgocQuach <ngocquach43@gmail.com>
2024-09-09 22:24:51 +0700

    feat: test 2

---
+++ scripts/start_server.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Get the latest Docker image from ECR
+docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG
+
+# Stop and remove the existing container
+docker stop vocab-management-container || true
+docker rm vocab-management-container || true
+
+# Run the new Docker container
+docker run -d --name vocab-management-container -p 4030:4030 --env-file $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG
