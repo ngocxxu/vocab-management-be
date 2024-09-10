e32ec2abb343fa5f81d8f782790e666aed9b626d
bono43 <bono43@nexondv.com>
2024-09-10 11:53:47 +0700

    test 4

--- scripts/install_dependencies.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# No need to install Docker, it's already running
-# You can add any other dependencies if needed
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,8 +3,7 @@
 # Get the latest Docker image from ECR
 docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG
 
-# Stop and remove the existing container
-docker stop vocab-management-container || true
+# Remove the existing container
 docker rm vocab-management-container || true
 
 # Run the new Docker container
