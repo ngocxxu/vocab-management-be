ff83e9f7ab1baa958d7280484a6ca8aa45e65847
NgocQuach <ngocquach43@gmail.com>
2024-10-25 19:50:54 +0700

    remove api url

--- src/main.ts
+++ src/main.ts
@@ -36,10 +36,10 @@ app.use(helmet());
 app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ extended: true, limit: '30mb' }));
 
-app.use('/api/comment', authenticateToken, comment);
-app.use('/api/user', user);
-app.use('/api/vocab', authenticateToken, vocab);
-app.use('/api/vocabTrainer', authenticateToken, vocabTrainer);
+app.use('/comment', authenticateToken, comment);
+app.use('/user', user);
+app.use('/vocab', authenticateToken, vocab);
+app.use('/vocabTrainer', authenticateToken, vocabTrainer);
 
 const logger = winston.createLogger({
   level: 'info',
