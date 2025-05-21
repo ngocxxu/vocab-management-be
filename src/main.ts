3a8563df55e9775c48b436c3d1d88c9b5eacd434
bono-ste <phungbaongoc.quach@stengg.com>
2025-05-21 23:40:47 +0700

    feat: change api endpoint

--- src/main.ts
+++ src/main.ts
@@ -37,11 +37,11 @@ app.use(helmet());
 app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ extended: true, limit: '30mb' }));
 
-app.use('/api/comment', authenticateToken, comment);
-app.use('/api/user', user);
-app.use('/api/vocab', authenticateToken, vocab);
-app.use('/api/vocabTrainer', authenticateToken, vocabTrainer);
-app.use('/api/vocabSubject', authenticateToken, vocabSubject);
+app.use('/app1/comment', authenticateToken, comment);
+app.use('/app1/user', user);
+app.use('/app1/vocab', authenticateToken, vocab);
+app.use('/app1/vocabTrainer', authenticateToken, vocabTrainer);
+app.use('/app1/vocabSubject', authenticateToken, vocabSubject);
 
 const logger = winston.createLogger({
   level: 'info',
